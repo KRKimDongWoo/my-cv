ab52a20e976de6394651dfaffb078bc65c980406
Ubuntu <ubuntu@ip-172-31-16-33.ap-northeast-2.compute.internal>
2020-03-10 11:44:51 +0000

    Updates

---
+++ precache-manifest.86a3a0a735b027db0fb3006c8cd4b513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd97bbeabd5910eea224a1dc0daeab95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36309e9614ae3d5a384b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ae45d94ac51bfde4cf73",
+    "url": "/static/js/2.9802615a.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.9802615a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36309e9614ae3d5a384b",
+    "url": "/static/js/main.bc3caf61.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  }
+]);
